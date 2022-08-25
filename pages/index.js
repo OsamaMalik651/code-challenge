00ffa9fc4f7cf4cd08f1cd158bfa341e325730cd
OsamaMalik651 <osamamalik124@outlook.com>
2022-08-25 15:11:41 -0600

    get all the post from api

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,35 @@
-
 import Head from 'next/head'
-import Image from 'next/image'
-
-const Home = () => {
+import Link from 'next/link'
+const Home = (props) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-start py-2">
       <Head>
         <title>Code Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="">
-        Code Challenge
-      </div>
+      <main>
+        <div className="">
+          <div className="">Search Bar</div>
+          <div className="">Posts component
+            {props.posts?.map(post => {
+              return <div className="" key={post.id}>
+                <Link href={`/post/${post.id}`}>{post.title}</Link>
+              </div>
+            })}
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
-
+export const getStaticProps = async () => {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then(
+    res => res.json()
+  )
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
 export default Home
